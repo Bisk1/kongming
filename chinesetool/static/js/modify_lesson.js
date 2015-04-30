a56e47ff9d9356fc36a5068bf63d9b5e62ca3d7a
MateuszMaluha <mateusz.maluha@gmail.com>
2015-05-01 00:07:41 +0200

    js script which adds new exercise without overloading

--- chinesetool/static/js/modify_lesson.js
+++ chinesetool/static/js/modify_lesson.js
@@ -1,51 +1,22 @@
-$(document).ready(function() {
+    $("#save").click(function(){
+        var url = $("#adding_exercise").attr("action");
+        var lesson = $("#adding_exercise").attr("lesson");
+        var formData = {};
+        $("#adding_exercise").find("input[name]").each(function (index, node) {
+            formData[node.name] = node.value;
+        });
+        $("#adding_exercise").find("textarea[name]").each(function (index, node) {
+            formData[node.name] = node.value;
+        });
+        console.log("my object: %o", formData);
+       $.ajax({
+            url : url,
+            type: "POST",
+            data : formData,
+            success: function() {
+                var link = $("#exercises").attr("link");
+                $("#exercises").load(link);
 
-    function FieldCounter() {
-        this.count = 1; // initial input boxes count
-        this.max = 10; // maximum input boxes count allowed
-        this.isFull = function() {
-            return (this.count >= this.max);
-        }
-    }
-
-    var addFieldHandler = function(e, wrapper, counter, input) {
-        e.preventDefault();
-        if(!counter.isFull()){
-            $(wrapper).append('<div>' + input + '<a href="#" class="remove_field">Remove</a></div>'); //add input box
-            counter.count++;
-        }
-    };
-
-    var removeFieldHandler = function(e, element, counter) {
-        e.preventDefault(); element.parent('div').remove();
-        counter.count--;
-    }
-
-    var wordZhWrapper        = $(".word_zh_fields_wrap");
-    var wordZhAddButton      = $(".add_word_zh_button");
-    var wordZhFieldCounter   = new FieldCounter();
-    var wordZhInput          = '<input type="text" name="word_zh[]"/>';
-
-    $(wordZhAddButton).click(function(e){
-        addFieldHandler(e, wordZhWrapper, wordZhFieldCounter, wordZhInput);
-    })
-
-    $(wordZhWrapper).on("click",".remove_field", function(e) {
-        removeFieldHandler(e, $(this), wordZhFieldCounter);
-    })
-
-    var wordReqWrapper        = $(".requirement_fields_wrap");
-    var wordReqAddButton      = $(".add_requirement_button");
-    var wordReqFieldCounter   = new FieldCounter();
-    var wordReqInput          = '<input type="text" name="requirement[]"/>';
-
-
-    $(wordReqAddButton).click(function(e){
-        addFieldHandler(e, wordReqWrapper, wordReqFieldCounter, wordReqInput);
-    })
-
-    $(wordReqWrapper).on("click",".remove_field", function(e) {
-        removeFieldHandler(e, $(this), wordReqFieldCounter);
-    })
-
-});
+            },
+        });
+    });
