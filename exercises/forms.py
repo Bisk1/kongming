fbe77c40f84311e3354cae17498eb8c771852833
Bisk <bisker@op.pl>
2015-08-17 09:23:40 +0200

    Use Django form for explanation

---
+++ exercises/forms.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+from models import Explanation
+
+
+class ExplanationForm(forms.ModelForm):
+
+    class Meta:
+        model = Explanation
+        fields = ['text', 'image']
+        labels = {
+            'text': _('Treść'),
+            'image': _('Obraz')
+        }
+
+
+
--- exercises/views.py
+++ exercises/views.py
@@ -5,8 +5,9 @@ from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
 from django.shortcuts import render, redirect
 
-from models import Lesson, Exercise, Explanation, ExerciseType, Typing
-from translations.models import TextTranslation, TextZH, TextPL
+from models import Lesson, Exercise, ExerciseType, Typing
+from translations.models import TextZH, TextPL
+from forms import ExplanationForm
 
 
 logger = logging.getLogger(__name__)
@@ -92,11 +93,11 @@ def handle_typing_spec(request, lesson, source_model, target_model, typing_spec)
 def add_explanation_exercise(request, lesson_id):
     lesson = Lesson.objects.get(id=lesson_id)
     exercise = Exercise()
-    explanation_spec = Explanation()
     if request.method == 'POST':
-        return handle_explanation_spec(request, lesson, exercise, explanation_spec)
+        return handle_explanation_spec(request, lesson, exercise)
     else:
-        return render(request, 'exercises/explanation.html', {'lesson': lesson})
+        form = ExplanationForm()
+        return render(request, 'exercises/explanation.html', {'lesson': lesson, 'form': form})
 
 
 def modify_explanation_exercise(request, lesson_id, exercise_id):
@@ -113,28 +114,18 @@ def modify_explanation_exercise(request, lesson_id, exercise_id):
     if request.method == 'POST':
         return handle_explanation_spec(request, lesson, exercise, explanation_spec)
     else:
-        return render(request, 'exercises/explanation.html', {'lesson': lesson, 'exercise': exercise})
+        form = ExplanationForm(instance=explanation_spec)
+        return render(request, 'exercises/explanation.html', {'lesson': lesson, 'exercise': exercise, 'form': form})
 
 
-def handle_explanation_spec(request, lesson, exercise, explanation_spec):
-    explanation_spec.text = request.POST.get('text')
-    if 'file' in request.FILES:
-        image_file = request.FILES['file']
-        image_file.name = save_image_for_exercise(image_file)
-        explanation_spec.image = image_file
-    explanation_spec.save()
-    exercise.lesson = lesson
-    exercise.spec = explanation_spec
-    exercise.save()
-    return redirect('lessons:modify_lesson', lesson_id=lesson.id)
+def handle_explanation_spec(request, lesson, exercise, explanation_spec=None):
+    form = ExplanationForm(request.POST, request.FILES, instance=explanation_spec)
+    if form.is_valid():
+        explanation_spec = form.save()
+        exercise.lesson = lesson
+        exercise.spec = explanation_spec
+        exercise.save()
+        return redirect('lessons:modify_lesson', lesson_id=lesson.id)
+    else:
+        return render(request, 'exercises/explanation.html', {'lesson': lesson, 'exercise': exercise, 'form': form})
 
-
-def save_image_for_exercise(file_to_save):
-    """
-    Generates an unique name for image and saves it in MEDIA_ROOT directory.
-    :param file: file to save
-    :return: name of the new saved file
-    """
-    name = unicode(uuid.uuid4()) + "." + file_to_save.name.split(".")[-1]
-    default_storage.save(name, ContentFile(file_to_save.read()))
-    return name
