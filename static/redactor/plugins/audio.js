c3edca6226eb7727244c401ea82f9629f84f3b20
Daniel Dylag <bisker@op.pl>
2016-02-19 16:30:18 +0100

    Live recording - first version [WIP]

--- static/redactor/plugins/audio.js
+++ static/redactor/plugins/audio.js
@@ -3,12 +3,52 @@
 	$.Redactor.prototype.audio = function()
 	{
 		return {
+
+            beforeStart: function()
+            {
+                if (recorder === undefined) {
+                    navigator.getUserMedia({audio: true}, startUserMedia, function(e) {
+                      __log('No live audio input: ' + e);
+                    });
+                }
+                $('#direct-recording').html('<button id="record-start">Start</button>');
+                $('#record-start').click(this.audio.started);
+            },
+            started: function()
+            {
+                $('#record-start').off();
+                recorder.clear();
+                recorder.record();
+                $('#direct-recording').html('<button id="record-stop">Stop</button>');
+                $('#record-stop').click(this.audio.stopped);
+                __log('Recording started');
+
+            },
+            stopped: function()
+            {
+                $('#record-stop').off();
+                __log('Recording stop triggered');
+                recorder.stop();
+                $('#direct-recording').html('<button id="record-confirm">Confirm</button><button id="record-cancel">Cancel</button>');
+                $('#record-confirm').click(this.audio.upload);
+                $('#record-cancel').click(this.audio.beforeStart);
+                __log('Recording stopped');
+            },
+            upload: function()
+            {
+                __log('Upload triggered');
+                recorder.exportWAV(this.upload.traverseFile);
+            },
+
 			getTemplate: function()
 			{
 				return String()
-                    + '<section id="redactor-modal-audio-insert">'
-                        + '<div id="redactor-modal-audio-droparea"></div>'
-                    + '</section>';
+				    + '<div id="upload-recording">'
+                        + '<section id="redactor-modal-audio-insert">'
+                            + '<div id="redactor-modal-audio-droparea"></div>'
+                        + '</section>'
+                    + '</div>'
+                    + '<div id="direct-recording"></div>'
 			},
 			init: function()
 			{
@@ -26,13 +66,16 @@
                 this.selection.save();
 				this.modal.show();
 
+				this.audio.beforeStart();
+
 			},
 			insert: function(json, direct, e)
             {
                 var $aud;
                 {
                     $aud = $('<audio>');
-                    $aud.prop('controls').attr('data-redactor-inserted-audio', 'true');
+                    $aud.prop('controls', true);
+                    $aud.attr('data-redactor-inserted-audio', 'true');
                     $source = $('<source>').attr('src', json.filelink).attr('type', 'audio/wav');
                     $source.text('Your browser does not support the audio element.');
                     $aud.append($source);
@@ -78,3 +121,38 @@
 		}
 	}
 })(jQuery);
+
+function __log(e, data) {
+    console.log("\n" + e + " " + (data || ''));
+}
+
+function startUserMedia(stream) {
+    __log('startUserMedia.');
+    var input = audio_context.createMediaStreamSource(stream);
+    __log('Media stream created.');
+
+    // Uncomment if you want the audio to feedback directly
+    //input.connect(audio_context.destination);
+    //__log('Input connected to audio context destination.');
+
+    recorder = new Recorder(input);
+    __log('Recorder initialised.');
+}
+
+var audio_context;
+var recorder;
+
+window.onload = function init() {
+    try {
+        // webkit shim
+        window.AudioContext = window.AudioContext || window.webkitAudioContext;
+        navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
+        window.URL = window.URL || window.webkitURL;
+
+        audio_context = new AudioContext;
+        __log('Audio context set up.');
+        __log('navigator.getUserMedia ' + (navigator.getUserMedia ? 'available.' : 'not present!'));
+    } catch (e) {
+        alert('No web audio support in this browser!');
+    }
+};
