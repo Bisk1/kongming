3b3efb82513dd0f0af140c0971baa8ae9e4c8034
Daniel Dylag <bisker@op.pl>
2016-03-04 17:10:00 +0100

    Move circle player generation from redactor field to a model helper.
     Optimize 'prepare' request handling

--- exercises/models.py
+++ exercises/models.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 
 import random
+import re
+from string import Template
 
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
-import re
 from redactor.fields import RedactorField
 from django.template.loader import render_to_string
+
+from lxml import etree
 
 from lessons.models import Lesson
 from translations.comparators import texts_difference
@@ -49,11 +52,11 @@ class Typing(AbstractExercise):
                 'correct_translation': self.text_to_translate.translations.first().text}
 
     def prepare(self):
-        return {'text': ChineseHelper.render_chinese_to_html(self.text_to_translate.text),
-                'language': self.text_to_translate.language}
+        return {'language': self.text_to_translate.language}
 
     def render(self):
-        return render_to_string('learn/typing.html', self.prepare())
+        return render_to_string('learn/typing.html',
+                                {'text': ChineseHelper.render_chinese_to_html(self.text_to_translate.text)})
 
     def __str__(self):
         return 'Typing - [{0}] Text: {1} |' \
@@ -76,12 +79,12 @@ class Choice(AbstractExercise):
                 'correct_translation': self.correct_choice.text}
 
     def prepare(self):
-        return {'text': ChineseHelper.render_chinese_to_html(self.text_to_translate.text),
-                'choices': self._get_all_choices_in_random_order(),
-                'language': self.text_to_translate.language}
+        return {}
 
     def render(self):
-        return render_to_string('learn/choice.html', self.prepare())
+        return render_to_string('learn/choice.html',
+                                {'text': ChineseHelper.render_chinese_to_html(self.text_to_translate.text),
+                                 'choices': self._get_all_choices_in_random_order()})
 
     def __str__(self):
         return 'Choice - [{0}] Text: {1} |' \
@@ -114,10 +117,12 @@ class Explanation(AbstractExercise):
         raise Exception("Explanation has no check method")
 
     def prepare(self):
-        return {'text': ChineseHelper.render_chinese_to_html(self.text)}
+        return {}
 
     def render(self):
-        return render_to_string('learn/explanation.html', self.prepare())
+        rendered_text = ChineseHelper.render_chinese_to_html(self.text)
+        rendered_text = AudioHelper.render_audio_players(rendered_text)
+        return render_to_string('learn/explanation.html', {'text': rendered_text})
 
     def __str__(self):
         return "Explanation - " + str(self.text)
@@ -135,10 +140,10 @@ class Listening(AbstractExercise):
                 'text': self.text.text}
 
     def prepare(self):
-        return {'audio_url': self.audio.url}
+        return {}
 
     def render(self):
-        return render_to_string('learn/listening.html', self.prepare())
+        return render_to_string('learn/listening.html', {'audio_url': self.audio.url})
 
     def __str__(self):
         return "Listening - " + str(self.text.text)
@@ -171,3 +176,48 @@ class ChineseHelper():
             word_zh = WordZH.get_or_create_with_translator(word=token)[0]
             wrapped += '<span class="chinese-word"><span>' + word_zh.pinyin + '</span><span>' + token + '</span></span>'
         return wrapped
+
+
+class AudioHelper():
+    audio_player_template = Template(
+        """<div id="$unique_id" class="cp-container" audio-src="$audio_src">
+             <div class="cp-jplayer"></div>
+             <div class="cp-buffer-holder cp-gt50" style="display: block;">
+                <div class="cp-buffer-1" style="transform: rotate(180deg);"></div>
+                <div class="cp-buffer-2" style="display: block; transform: rotate(271.362deg);"></div>
+            </div>
+            <div class="cp-progress-holder" style="display: block;">
+                <div class="cp-progress-1" style="transform: rotate(94.2428deg);"></div>
+                <div class="cp-progress-2" style="transform: rotate(0deg); display: none;"></div>
+            </div>
+            <div class="cp-circle-control"></div>
+            <ul class="cp-controls">
+                <li><a class="cp-play" tabindex="1" style="display: block;">play</a></li>
+                <li><a class="cp-pause" style="display: none;" tabindex="1">pause</a></li>
+            </ul>
+        </div>""".replace('\n', '')
+    )
+
+    @classmethod
+    def render_audio_players(cls, text_to_render):
+        parser = etree.HTMLParser()
+        root = etree.fromstring(text_to_render, parser)
+        regexpNS = 'http://exslt.org/regular-expressions'  # to enable regex
+        audio_link_elements = root.xpath("//a[re:test(@href, 'wav$')]", namespaces={'re': regexpNS})  # all wav files
+
+        for index, audio_link_element in enumerate(audio_link_elements):
+            audio_src = audio_link_element.attrib['href']
+            rendered_player = cls.audio_player_template.substitute(audio_src=audio_src,
+                                                                   unique_id='player-id-' + str(index))
+            player_element = etree.fromstring(rendered_player)
+            audio_link_element.getparent().replace(audio_link_element, player_element)
+        res = etree.tostring(root, encoding='unicode')
+        res = cls.unwrap(res)  # tree ends up wrapped in <html> and <body> elements that must be stripped
+        return res
+
+    @classmethod
+    def unwrap(cls, text):
+        if text.startswith("<html><body>") and text.endswith("</body></html>"):
+            return text[12:len(text) - 14]
+        else:
+            raise Exception("Unexpected form of rendered HTML")
--- static/redactor/plugins/audio.js
+++ static/redactor/plugins/audio.js
@@ -61,48 +61,14 @@
 				this.modal.addTemplate('audio', this.audio.getTemplate());
 
 				this.modal.load('audio', 'Audio', 700);
-				this.upload.init('#redactor-modal-audio-droparea', this.opts.fileUpload, this.audio.insert);
+				this.upload.init('#redactor-modal-audio-droparea', this.opts.fileUpload, this.file.insert);
 
                 this.selection.save();
 				this.modal.show();
 
 				this.audio.beforeStart();
 
-			},
-			insert: function(json, direct, e)
-            {
-                var audio = $(String()
-                + '<div class="cp-container" audio-src="' + json.filelink + '">'
-                    + '<div class="cp-jplayer"></div>'
-                    + '<div class="cp-buffer-holder cp-gt50" style="display: block;">'
-                        + '<div class="cp-buffer-1" style="transform: rotate(180deg);"></div>'
-                        + '<div class="cp-buffer-2" style="display: block; transform: rotate(271.362deg);"></div>'
-                    + '</div>'
-                    + '<div class="cp-progress-holder" style="display: block;">'
-                        + '<div class="cp-progress-1" style="transform: rotate(94.2428deg);"></div>'
-                        + '<div class="cp-progress-2" style="transform: rotate(0deg); display: none;"></div>'
-                    + '</div>'
-                    + '<div class="cp-circle-control"></div>'
-                    + '<ul class="cp-controls">'
-                        + '<li><a class="cp-play" tabindex="1" style="display: block;">play</a></li>'
-                        + '<li><a class="cp-pause" style="display: none;" tabindex="1">pause</a></li>'
-                    + '</ul>'
-                + '</div>');
-
-                audio.uniqueId();
-
-                this.modal.close();
-                this.selection.restore();
-                this.buffer.set();
-
-                this.insert.html(this.utils.getOuterHtml(audio), false);
-
-                new CirclePlayer("#" + audio.attr("id"));
-
-                if (typeof json == 'string') return;
-                this.core.setCallback('fileUpload', audio, json);
-
-            }
+			}
 		}
 	}
 })(jQuery);
