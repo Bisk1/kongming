75af9ccbdb2a2eeafd6242e9cbb6b403fc92c5d3
Bisk <bisker@op.pl>
2016-07-24 11:15:13 +0200

    Add JS form engine and refactor audio plugin

---
+++ static/js/form-engine.js
@@ -0,0 +1,42 @@
+var FormEngine = {
+    renderField: function (fieldSchema) {
+        switch (fieldSchema.type) {
+            case "text":
+                return '<div id="div_' + fieldSchema.id + '" class="form-group">'
+                    + '<label for="recording-text"  class="control-label col-lg-4">' + fieldSchema.label + '</label>'
+                    + '<div class="col-lg-8">'
+                    + '<input class="textinput textInput form-control" id="' + fieldSchema.id + '" name="text" placeholder="'
+                    + fieldSchema.placeholder + '" type="text">'
+                    + '</div></div>';
+            case "emptyDiv":
+                return '<div id="' + fieldSchema.id + '"></div>';
+            case "customHtml":
+                return fieldSchema.html;
+            default:
+                throw "Unsupported field type: " + fieldSchema.type;
+        }
+    },
+    renderForm: function (formSchema) {
+        var color = formSchema.hasOwnProperty("color") ? formSchema.color : "blue";
+        var title = formSchema.title;
+        var result = String()
+            + '<form class="form-horizontal">'
+            + '<div class="col-md-12">'
+            + '<div class="widget w' + color + '"><div class="widget-head">'
+            + '<div class="pull-left">' + title + '</div>'
+            + '<div class="clearfix"></div>'
+            + '</div></div>'
+            + '<div class="widget-content"><div class="pad">';
+        if (formSchema.hasOwnProperty("fields")) {
+            for (var i = 0; i < formSchema.fields.length; i++) {
+                result += this.renderField(formSchema.fields[i]);
+            }
+        }
+        result += '</div></div></div></form>';
+        return result;
+    },
+
+    button: function (buttonSchema) {
+        return '<button type="button" class="btn btn-default" id="' + buttonSchema.id + '">' + buttonSchema.text + '</button>';
+    }
+};
--- static/redactor/plugins/audio.js
+++ static/redactor/plugins/audio.js
@@ -1,28 +1,24 @@
 var audioContext;
 var recorder;
 
-(function($)
-{
-	$.Redactor.prototype.audio = function()
-	{
-		return {
+(function ($) {
+    $.Redactor.prototype.audio = function () {
+        return {
 
-            beforeStart: function()
-            {   // microphone will work only over secure connection (HTTPS) or on localhost (for testing purposes)
+            beforeStart: function () {   // microphone will work only over secure connection (HTTPS) or on localhost (for testing purposes)
                 if (window.location.protocol == "https:" || document.location.hostname == "localhost") {
-                    $('#direct-recording').html('<button type="button" class="btn btn-default" id="record-start">Start</button>');
+                    $('#direct-recording').html(FormEngine.button({id: "record-start", text: "Start"}));
                     $('#record-start').click(this.audio.startRequested);
                 } else {
                     $('#direct-recording').html('<h3>Only available over HTTPS!</h3>');
                 }
             },
-            startRequested: function()
-            {
+            startRequested: function () {
                 $('#record-start').off();
                 var afterSetup = this.audio.started;
                 if (recorder === undefined) {
                     navigator.getUserMedia({audio: true},
-                        function(stream) {
+                        function (stream) {
                             var input = audioContext.createMediaStreamSource(stream);
                             console.log('Media stream created.');
                             // Uncomment if you want the audio to feedback directly
@@ -33,51 +29,48 @@ var recorder;
                             console.log('Recorder initialised.');
                             afterSetup();
                         },
-                        function(e) {
-                          console.log('No live audio input: ' + e);
+                        function (e) {
+                            console.log('No live audio input: ' + e);
                         });
                 } else {
                     afterSetup();
                 }
             },
-            started: function()
-            {
+            started: function () {
                 recorder.clear();
                 recorder.record();
-                $('#direct-recording').html('<button type="button" class="btn btn-default" id="record-stop">Stop</button>');
+                $('#direct-recording').html(FormEngine.button({id: "record-stop", text: "Stop"}));
                 $('#record-stop').click(this.audio.stopped);
                 console.log('Recording started');
 
             },
-            stopped: function()
-            {
+            stopped: function () {
                 $('#record-stop').off();
                 recorder.stop();
-                $('#direct-recording').html('<button type="button" class="btn btn-default" id="record-confirm">Confirm</button>' +
-                '<button type="button" class="btn btn-default" id="record-play">Play</button>' +
-                '<button type="button" class="btn btn-default" id="record-cancel">Cancel</button>');
+                $('#direct-recording').html(
+                    FormEngine.button({id: "record-confirm", text: "Confirm"}) +
+                    FormEngine.button({id: "record-play", text: "Play"}) +
+                    FormEngine.button({id: "record-cancel", text: "Cancel"})
+                );
                 $('#record-confirm').click(this.audio.export);
                 $('#record-play').click(this.audio.playRequested);
                 $('#record-cancel').click(this.audio.beforeStart);
                 console.log('Recording stopped');
             },
-            playRequested: function()
-            {
+            playRequested: function () {
                 recorder.getBuffer(this.audio.play);
             },
-            play: function(buffers)
-            {
+            play: function (buffers) {
                 var newSource = audioContext.createBufferSource();
-                var newBuffer = audioContext.createBuffer( 2, buffers[0].length, audioContext.sampleRate );
+                var newBuffer = audioContext.createBuffer(2, buffers[0].length, audioContext.sampleRate);
                 newBuffer.getChannelData(0).set(buffers[0]);
                 newBuffer.getChannelData(1).set(buffers[1]);
                 newSource.buffer = newBuffer;
-                newSource.connect( audioContext.destination );
+                newSource.connect(audioContext.destination);
                 newSource.start(0);
             },
-            export: function()
-            {
-                var name = $('#recording_text').val();
+            export: function () {
+                var name = $('#recording-text').val();
                 if (name == undefined) {
                     alert("You must provide recording text");
                     recorder.exportWAV(this.audio.stopped);
@@ -85,67 +78,60 @@ var recorder;
                 console.log('Export triggered');
                 recorder.exportWAV(this.audio.upload);
             },
-            upload: function(blob)
-            {
-                var name = $('#recording_text').val();
+            upload: function (blob) {
+                var name = $('#recording-text').val();
                 console.log('Upload triggered for ' + name);
-                file = new File([blob], name + '.wav');
+                var file = new File([blob], name + '.wav');
                 this.upload.traverseFile(file);
             },
-			getTemplate: function()
-			{
-                // TODO: add template engine and refactor
-				return String()
-                    + '<form class="form-horizontal">'
-                    + '<div class="col-md-12">'
-                    + '<div class="widget wgreen"> <div class="widget-head">'
-                    + '<div class="pull-left">A) Direct recording</div>'
-                    + '<div class="clearfix"></div>'
-                    + '</div></div>'
-                    + '<div class="widget-content"><div class="pad">'
-                    + '<div id="div_recording_text" class="form-group">'
-                    + '<label for="recording_text"  class="control-label col-lg-4">Text</label>'
-                    + '<div class="col-lg-8">'
-                    + '<input class="textinput textInput form-control" id="recording_text" name="text" placeholder="The text in the recording" type="text">'
-                    + '</div>'
-                    + '</div>'
-                    + '<div id="direct-recording"></div>'
-                    + '</div></div>'
-                    + '</div>'
-                    + '<div class="col-md-12">'
-                    + '<div class="widget worange"> <div class="widget-head">'
-                    + '<div class="pull-left">B) File upload</div>'
-                    + '<div class="clearfix"></div>'
-                    + '</div></div>'
-				    + '<div id="upload-recording">'
-                        + '<section id="redactor-modal-audio-insert">'
+            getTemplate: function () {
+                return String()
+                    + FormEngine.renderForm({
+                        title: "A) Direct recording",
+                        color: "green",
+                        fields: [{
+                            type: "text",
+                            id: "recording-text",
+                            label: "Text",
+                            placeholder: "The text in the recording"
+                        }, {
+                            type: "emptyDiv",
+                            id: "direct-recording"
+                        }]
+                    })
+                    + FormEngine.renderForm({
+                        title: "B) File upload",
+                        color: "orange",
+                        fields: [{
+                            type: "customHtml",
+                            html: '<div id="upload-recording">'
+                            + '<section id="redactor-modal-audio-insert">'
                             + '<div id="redactor-modal-audio-droparea"></div>'
-                        + '</section>'
-                    + '</div>'
-                    + '</div>'
-                    + '</form>'
-			},
-			init: function()
-			{
-				var button = this.button.addAfter('image', 'audio', "Insert Audio...");
-				button.attr("class", "icon-music redactor-btn-image");
-				this.button.addCallback(button, this.audio.show);
-			},
-			show: function()
-			{
-				this.modal.addTemplate('audio', this.audio.getTemplate());
+                            + '</section>'
+                            + '</div>'
+                        }]
+                    });
 
-				this.modal.load('audio', 'Audio', 700);
-				this.upload.init('#redactor-modal-audio-droparea', this.opts.fileUpload, this.file.insert);
+            },
+            init: function () {
+                var button = this.button.addAfter('image', 'audio', "Insert Audio...");
+                button.attr("class", "icon-music redactor-btn-image");
+                this.button.addCallback(button, this.audio.show);
+            },
+            show: function () {
+                this.modal.addTemplate('audio', this.audio.getTemplate());
+
+                this.modal.load('audio', 'Audio', 700);
+                this.upload.init('#redactor-modal-audio-droparea', this.opts.fileUpload, this.file.insert);
 
                 this.selection.save();
-				this.modal.show();
+                this.modal.show();
 
-				this.audio.beforeStart();
+                this.audio.beforeStart();
 
-			}
-		}
-	}
+            }
+        }
+    }
 })(jQuery);
 
 
